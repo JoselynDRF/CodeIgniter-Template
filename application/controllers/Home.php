bc279b08f9a636259ea284819e054250d6a74b91
JoselynDRF <mariajoselyndosramos@yahoo.es>
2018-03-09 16:50:50 +0000

    Create first model

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	function __construct() {
+		parent::__construct();
+		$this->load->model('home_model');
+	}
+
 	public function index()
 	{
-		$this->load->view('home');
+		$data['title'] = 'CodeIgniter - Template'; 
+		$data['description'] = 'Welcome to CodeIgniter';
+
+		// Load data from Model
+		$dataFromModel = $this->home_model->getData();
+		$data['dataFromModel'] = $dataFromModel;
+
+		// Send to View
+		$this->load->view('home', $data);
 	}
 }
--- application/views/home.php
+++ application/views/home.php
@@ -3,11 +3,11 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title> CodeIgniter - Template </title>
+		<title> <?= $title ?> </title>
 	</head>
 
 	<body>
-		<h1> Welcome to CodeIgniter </h1>
+		<h1> <?= $description ?> </h1>
+		<h2> <?= $dataFromModel ?> </h2>
 	</body>
 </html>
